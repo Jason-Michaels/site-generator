4b141660d8ededb11d9f1de9eb3a951e1b19e7a3
Jason Michaels <jason.michaels721@gmail.com>
2019-09-18 17:14:34 +0000

    new web engine

--- index.py
+++ index.py
@@ -1,26 +1,9 @@
-import falcon
-import os
-import json
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
 
 
-class ThingsResource(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = '{"hello":"world"}'
+app = Starlette(debug=True)
 
-class TestStuff(object):
-    def on_get(self, req, resp):
-        get_os_value = {'test': os.getenv('MY_VARIABLE', 'no_value'), 'test_2': os.getenv('another_one', 'no_value')}
-        resp.body = json.dumps(get_os_value)
-
-
-# falcon.API instances are callable WSGI apps
-app = falcon.API()
-
-# Resources are represented by long-lived class instances
-things = ThingsResource()
-
-# things will handle all requests to the '/' URL path
-app.add_route('/', things)
-app.add_route('/test_stuff_2', TestStuff())
+@app.route('/')
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
