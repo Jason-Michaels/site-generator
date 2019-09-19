3c6d0bba2bfdf9471738c594b709caa0c4f2f648
Jason Michaels <jason.michaels721@gmail.com>
2019-09-19 19:03:55 +0000

    trying out now routing.

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.views import CompositionView
+from sanic.response import text
 
-app = Sanic()
+app = Sanic(__name__)
 
-app.static('/', './')
+def get_handler(request):
+    return text('I am a get method')
+
+view = CompositionView()
+view.add(['GET'], get_handler)
+view.add(['POST', 'PUT'], lambda request: text('I am a post/put method'))
+
+# Use the new view to handle requests to the base URL
+app.add_route(view, '/api')
+
+app.static('/', './frontend/mysite.io/public')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
