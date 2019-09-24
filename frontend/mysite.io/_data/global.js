e4ae4a1bd217d61416cd6e146fcac32bfd78f716
Jason Michaels <jason.michaels721@gmail.com>
2019-09-24 09:57:33 +0000

    testing api

--- frontend/mysite.io/_data/global.js
+++ frontend/mysite.io/_data/global.js
@@ -1,7 +1,22 @@
+let axios = require('axios');
 require('dotenv').config();
+
+function fetchSiteData(path) {
+    return axios.get(process.env.API + path)
+        .then(function (response) {
+            console.log(response.data);
+            return response.data;
+        })
+        .catch(function(error) {
+            console.log(error)
+            return {'error': 'see logs'}
+        })
+
+}
 
 module.exports = {
    my_var: process.env.test2 || 'no var',
+   site_data: fetchSiteData('sheet1'),
    currentDate: function() {
     // You can have a JavaScript function here!
     return (new Date()).toLocaleString();
